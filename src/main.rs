4dd91edc2fffbc7beba6576f31e131fa266e1880
Loc Mai <locmai@gmail.com>
2024-10-08 17:17:42 +0700

    refactor: metric_handler

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,13 @@ use std::fs;
 use clap::Parser;
 use serde_yaml;
 use crate::config::Config;
-use tokio::time::{self, Duration};
 
-use axum::body::Body;
-use axum::extract::State;
-use axum::http::header::CONTENT_TYPE;
-use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::Router;
-use prometheus_client::encoding::text::encode;
 use prometheus_client::registry::Registry;
-use prometheus_client::encoding::{EncodeLabelValue,EncodeLabelSet};
-use prometheus_client::metrics::family::Family;
-use prometheus_client::metrics::counter::Counter;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-
-use metrics::fetch_metrics;
+use metrics::{initialize_registry, metrics_handler};
 
 mod tsdb;
 mod metrics;
@@ -40,40 +29,10 @@ fn load_config(filename: &str) -> Config {
         .expect("Failed to parse the configuration file")
 }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
-pub enum Method {
-    Get,
-    Post,
-}
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
-pub struct MethodLabels {
-    pub method: Method,
-}
-
-#[derive(Debug)]
-pub struct Metrics {
-    requests: Family<MethodLabels, Counter>,
-}
-
 #[derive(Debug)]
 pub struct AppState {
     pub registry: Registry,
-}
-
-pub async fn metrics_handler(State(state): State<Arc<Mutex<AppState>>>) -> impl IntoResponse {
-    let state = state.lock().await;
-    let mut buffer = String::new();
-    encode(&mut buffer, &state.registry).unwrap();
-
-    Response::builder()
-        .status(StatusCode::OK)
-        .header(
-            CONTENT_TYPE,
-            "application/openmetrics-text; version=1.0.0; charset=utf-8",
-        )
-        .body(Body::from(buffer))
-        .unwrap()
+    pub config: Config,
 }
 
 #[tokio::main]
@@ -81,35 +40,13 @@ async fn main() {
     let args = Args::parse();
     let config = load_config(&args.config);
     println!("Loaded configuration: {:?}", config);
-    
-    //for instance in config.prometheus.instances {
-    //    tokio::spawn(async move {
-    //        let mut interval = time::interval(Duration::from_secs(15));
-    //        loop {
-    //            interval.tick().await;
-    //            match fetch_metrics(&instance.url) {
-    //                Ok(metrics) => {
-    //                    println!("Metrics from {}: {}", &instance.name.to_string(), metrics.status);
-    //                    println!("Metrics from {}: {}", &instance.name.to_string(), metrics.data.series_count_by_metric_name[0].name);
-    //
-    //                }
-    //                Err(e) => {
-    //                    eprintln!("Error fetching metrics from {}: {}", &instance.name, e);
-    //                }
-    //            }
-    //        }
-    //    });
-    //};
 
-    let metrics = Metrics {
-        requests: Family::default(),
-    };
     let mut state = AppState {
         registry: Registry::default(),
+        config,
     };
-    state
-        .registry
-        .register("requests", "Count of requests", metrics.requests.clone());
+    state = initialize_registry(state).unwrap(); 
+
     let state = Arc::new(Mutex::new(state));
     let router = Router::new()
         .route("/metrics", get(metrics_handler))
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,6 +1,43 @@
 use reqwest::blocking::Client;
 use reqwest::Error;
 use crate::tsdb::TsdbStatus;
+use crate::AppState;
+use prometheus_client::metrics::family::Family;
+use prometheus_client::metrics::counter::Counter;
+use prometheus_client::encoding::{EncodeLabelSet, EncodeLabelValue};
+use prometheus_client::encoding::text::encode;
+use axum::body::Body;
+use axum::extract::State;
+use axum::http::header::CONTENT_TYPE;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
+pub enum Method {
+    Get,
+    Post,
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
+pub struct MethodLabels {
+    pub method: Method,
+}
+
+#[derive(Debug)]
+pub struct Metrics {
+    requests: Family<MethodLabels, Counter>,
+}
+
+
+pub fn initialize_registry(mut app_state: AppState) -> Result<AppState, Error>{
+    let metrics = Metrics {
+        requests: Family::default(),
+    };
+    app_state.registry.register("requests", "Count of requests", metrics.requests.clone());
+    return Ok(app_state)
+}
 
 pub fn fetch_metrics(url: &str) -> Result<TsdbStatus, Error> {
     let client = Client::new();
@@ -13,3 +50,20 @@ pub fn fetch_metrics(url: &str) -> Result<TsdbStatus, Error> {
 
     Ok(tsdb_status)
 }
+
+pub async fn metrics_handler(State(state): State<Arc<Mutex<AppState>>>) -> impl IntoResponse {
+    let state = state.lock().await;
+    let mut buffer = String::new();
+    encode(&mut buffer, &state.registry).unwrap();
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header(
+            CONTENT_TYPE,
+            "application/openmetrics-text; version=1.0.0; charset=utf-8",
+        )
+        .body(Body::from(buffer))
+        .unwrap()
+}
+
+
